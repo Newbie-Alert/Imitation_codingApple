a15fb7bc2f5e86f03ce2196c0a3daf3aec2f5546
NebieAlert <mjnn100801@naver.com>
2023-06-23 15:37:10 +0900

    main 반응형 적용, 두번째 단 제작

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import styles from "./Home.module.css";
 
 // data
 import pills from "./homePillsData";
+import course from "./homeEduData";
 
 function Home() {
   return (
     <main className={`${styles.main_page_container}`}>
+      {/* MAIN_SECTION_FIRST */}
       <div className={`${styles.main_section_one} ${styles.container}`}>
-        {/* MAIN_SECTION ONE_TEXTS */}
+        {/* MAIN_SECTION_FIRST_TEXTS */}
         <div className={styles.section_one_texts}>
           <div className={styles.section_one_texts_title}>
             <h1>포트폴리오 완성까지 책임지는</h1>
@@ -31,7 +33,7 @@ function Home() {
           </div>
           <button className={styles.main_button}>강의 목록 보기</button>
         </div>
-        {/* MAIN_SECTION ONE_IMAGES */}
+        {/* MAIN_SECTION_FIRST_IMAGES */}
         <div className={styles.section_one_images}>
           <div className={`${styles.bubble1} ${styles.bubble}  `}>
             <img src={process.env.PUBLIC_URL + "main1.jpg"} alt="" />
@@ -44,6 +46,45 @@ function Home() {
           </div>
         </div>
       </div>
+
+      {/* MAIN_SECTION_SECOND */}
+      <section
+        className={`${styles.section_second_container} ${styles.container}`}
+      >
+        <div className={styles.section_second}>
+          <div className={styles.section_second_top}>
+            <h3>
+              수박코딩 웹개발 <span>프리패스</span>
+            </h3>
+            <img src={process.env.PUBLIC_URL + "present.png"} alt="" />
+          </div>
+          <div className={styles.section_second_course}>
+            {course.map((el, i) => {
+              return (
+                <div className={styles.course_item} key={i}>
+                  <img src={el.image} alt="" />
+                  <h6>{el.name}</h6>
+                  <h6>{el.secondName}</h6>
+                </div>
+              );
+            })}
+          </div>
+          <div className={styles.section_second_bottom}>
+            <div className={styles.section_second_price}>
+              <h4>
+                <span>￦</span>482,000
+              </h4>
+              <h3>
+                <span>￦</span>230,000
+              </h3>
+            </div>
+            <div className={styles.section_second_description}>
+              <p>월 1만 9천원에 즐기는 프론트/백엔드 웹개발 풀코스</p>
+              <h1>프리패스 Essential</h1>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
---
+++ src/Pages/Home/homeEduData.js
@@ -0,0 +1,43 @@
+let course = [
+  {
+    id: 0,
+    name: 'HTML',
+    secondName: 'CSS',
+    image: process.env.PUBLIC_URL + "html.png"
+  },
+  {
+    id: 1,
+    name: 'JavaScript',
+    secondName: '기초',
+    image: process.env.PUBLIC_URL + "js.png"
+  },
+  {
+    id: 2,
+    name: 'React',
+    image: process.env.PUBLIC_URL + "react.png"
+  },
+  {
+    id: 3,
+    name: 'JavaScript',
+    secondName: 'ES6',
+    image: process.env.PUBLIC_URL + "js.png"
+  },
+  {
+    id: 4,
+    name: 'Node.js',
+    secondName: 'MongoDB',
+    image: process.env.PUBLIC_URL + "node.png"
+  },
+  {
+    id: 5,
+    name: 'Vue',
+    image: process.env.PUBLIC_URL + "vue.png"
+  },
+  {
+    id: 6,
+    name: 'TypeScript',
+    image: process.env.PUBLIC_URL + "ts.png"
+  },
+]
+
+export default course;
